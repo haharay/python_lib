757882cb2de8ece04d59b3b6b2587bb27446258f
haharay <hcmray@qq.com>
2021-04-08 20:44:42 +0800

    add

--- make_problem.py
+++ make_problem.py
@@ -1,77 +1,113 @@
+import os
+import re
+import pandas as pd
+import numpy as np
+from markdown import markdown
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+from subprocess import check_output
+#  ￥之间的为需要填写的，#%开头的为提示。
+#################################################
+prob = '''
+题目要求：
+编写一个Python程序，从example.com中抓取h1标签。
+
 '''
+###————————————————————————————————————————————————————
+code_source = """
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen('http://www.example.com/')
+bsh = ￥BeautifulSoup￥(html.read(), 'html.parser')
+print(bsh.h1)
+"""
+###########################################################
+###########################################################
+"""
+其它备注信息：
+1. with ... as ...语句用于方便的处理文件打开、读写、关闭过程中的异常。
+
 原文版本为 highlight.py
 简答题：参考文献：https://m.hanspub.org/journal/paper/37823 ALBERT等神经网络模型。
 论述题：EASE
-'''
-
-import os
-import re
-
-import numpy as np
-import pandas as pd
-
-#  ￥之间的为需要填写的，#%开头的为提示。不一定H3标题。
-#################################################
-prob = '''
-<h3>
-问题题干部分，丫丫
-</h3>\n'''
-########################
-code = """
-chipo.item_name.￥value_counts().count()￥
 """
 ######################################################################
 #exec(code.replace('￥', ''))
-ZZ = 0  # =0，表示不显示结果，等于array或pandas则显示结果。
+ZZ = 1  # =0，表示不执行程序；==1:执行程序，并显示结果；==2, 返回名为df或result的array或pandas对象时则显示结果。
 frmt = '{:1.0f}'  # 数组显示格式
 sol = ''  # 显示答案时的解析步骤
 
+
+
 ## 这部分设置一般不用修改
-expect = re.findall(r'￥(.*?)￥', code)[0]
-hints = re.findall(r'#%(.*?)\n', code)
+expect = re.findall(r'￥(.*?)￥', code_source)[0]
+hints = re.findall(r'#%(.*?)\n', code_source)
 ## 清掉答案和帮助文本
-code = re.sub(r'￥.*?￥', '_________________________', code)
+code = re.sub(r'￥.*?￥', '_________________________', code_source)
 code = re.sub(r'#%.*?\n', '\n', code)
 code = re.sub(r'"', "'", code)  # 以单引号代替双引号
-
+code_exec = re.sub(r'￥', '', code_source)
 
 ##############################################################
 ## 把数组和pandas转化为mathjax，用于显示。
 def array_disp(ZZ, frmt):
     import array_to_latex as a2l
     from bs4 import BeautifulSoup
-    if type(ZZ) == int:
+    global code_exec
+    if ZZ==0:
         return ''
-    else:
-        lx = a2l.to_ltx(ZZ, frmt=frmt, arraytype='bmatrix', print_out=False)
-        with open("练习NB/latex_txt.text", 'w') as fa:
+    elif ZZ==1:
+        with open("./test001.py", 'w') as fa:
+            fa.write(code_exec)
+        lines = check_output("python3 ./test001.py", shell=True).decode('utf-8')
+        lines = lines.replace('<', '&lt;')
+        lines = lines.replace('>', '&gt;')
+        lines = '执行上述程序，输出:\n'+lines
+        print(lines)
+        return markdown(lines.replace('\n', '<br/>'))
+    elif ZZ==2:
+        exec(code_exec)
+        if 'df' in locals():
+            df =df
+        elif 'result' in locals():
+            df = result
+        else:
+            print('找不到ndarray或pandas的结果变量')
+        lx = a2l.to_ltx(df, frmt=frmt,
+                            arraytype='bmatrix', print_out=False)
+        with open("./latex_txt.text", 'w') as fa:
             fa.write(lx)
         os.system('pandoc latex_txt.text -s --mathjax --metadata title="latexTxt" -o math_mathjax.html')
         # time.sleep(1.0)
-        with open('练习NB/math_mathjax.html', 'r') as fr:
+        with open('./math_mathjax.html', 'r') as fr:
             lines = fr.readlines()
         soup = BeautifulSoup(''.join(lines), 'html.parser')
         return str(soup.find_all('span')[0])
+    else:
+        return ''
 
 
 ## 处理答案和提示等部分
 def ans_hint(expect, sol, hints):
     ss = """
-<description>将程序中长下划线部分所缺的代码，填入下框中：</description>  
-<customresponse cfn="check_ans" expect="YYYY">
-    <textline size="30"/>
-</customresponse>
-"""
+    <description>将程序中长下划线部分所缺的代码，填入下框中（请使用单引号）：</description>  
+    <customresponse cfn="check_ans" expect="YYYY">
+        <textline size="30"/>
+    </customresponse>
+    """
     ss = ss.replace('YYYY', expect)
-    if len(sol) > 0:
-        ss = ss + """
-<solution>
+    detail_solution = """
+    <solution>
     <div class="detailed-solution">
-        <p>XXXXX</p>
+        <p>解析：</p>
+    <iframe src="https://trinket.io/embed/python3/9d578a67e3" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen="false"></iframe>
     </div>
-</solution>        
-        """
-        ss = ss.replace('XXXXX', sol)
+    </solution>
+    """
+    ss = ss + detail_solution
+    if len(sol) > 0:
+        ss = ss.replace('解析：', sol)
     if len(hints) > 0:
         ss = ss + '<demandhint>' + '\n'
         for ht in hints:
@@ -82,10 +118,6 @@ def ans_hint(expect, sol, hints):
 
 
 def main(code, prob, ZZ, frmt, expect, sol, hints):
-    from pygments import highlight
-    from pygments.formatters import HtmlFormatter
-    from pygments.lexers import PythonLexer
-
     ## 两个评分函数，一个比例评分，一个正误评分
     # to do: tfidf的中文评阅
     correct_grader = """
@@ -107,7 +139,9 @@ def check_ratio(expect, answer):
     """
 
     code_str = highlight(code, PythonLexer(), HtmlFormatter(noclasses=True))
-    ppp = correct_grader + prob + array_disp(ZZ, frmt) + code_str + ans_hint(expect, sol, hints)
+    prob = markdown(prob.replace('\n', '<br/>'))
+    ppp = correct_grader + prob + code_str +\
+        array_disp(ZZ, frmt) + ans_hint(expect, sol, hints)
     # print(ppp)
     try:
         import clipboard as _clipboard
